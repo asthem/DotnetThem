ac12b3a04e5f9439caa99c0ea2727d2442496c1d
Tian Zhao <kastrcn@outlook.com>
2020-10-13 11:05:44 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace them
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello them!");
+        }
+    }
+}
